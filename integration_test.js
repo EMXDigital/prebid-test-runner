60c53996fb5c792f27804b24a85a2673b9b6c57a
Kevin <kevin.hagens@brealtime.com>
2021-10-19 17:42:51 -0500

    change console error so appears in git actions

--- integration_test.js
+++ integration_test.js
@@ -13,44 +13,44 @@ const { json } = require('body-parser');
     if(request.url().toString().includes('hb.emxdgt.com')){
 
         //inspect url
-        console.log('EMX Header Bidding Request: ' + request.url());
+        console.error('EMX Header Bidding Request: ' + request.url());
 
         //inspect json body
         const hbRequest = JSON.parse(request.postData());
         
         //do validation logic here....
-        console.log('checking to ensure EMX header bidding request present');
+        console.error('checking to ensure EMX header bidding request present');
         if(!hbRequest){
           throw new Error('no header bidding request found');
         }
 
         
-        console.log('checking to ensure request has an id');
+        console.error('checking to ensure request has an id');
         if(!hbRequest.id){ //check id
           throw new Error('header bidding request must have an id');
         }
 
-        console.log('checking to ensure request has an imp object');
+        console.error('checking to ensure request has an imp object');
         if(!hbRequest.imp){ //check to make sure containts imp
           throw new Error('header bidding request must have an imp');
         }
 
-        console.log('checking to ensure imp object is an array');
+        console.error('checking to ensure imp object is an array');
         if(!Array.isArray(hbRequest.imp)){ // make sure that imp object is and array 
           throw new Error('imp object is not an array');
         }
 
-        console.log('checking to ensure imp array is not empty');
+        console.error('checking to ensure imp array is not empty');
         if(hbRequest.imp.length == 0){ //check it has at least one impression object
           throw new Error('imp array is empty')
         }
 
-        console.log('checking to ensure request is of type site or app');
+        console.error('checking to ensure request is of type site or app');
         if(!hbRequest.site && !hbRequest.app){ //check to ensure site object or app object, app for education, always site here
           throw new Error('header bidding request must have site or app object')
         }
 
-        console.log(JSON.stringify(hbRequest));
+        console.error(JSON.stringify(hbRequest));
         
         return;
 
