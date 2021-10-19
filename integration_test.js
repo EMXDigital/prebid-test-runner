fd8db79aa2f536747706cc5866096a9288568351
Kevin <kevin.hagens@brealtime.com>
2021-10-19 18:29:23 -0500

    add logs

--- integration_test.js
+++ integration_test.js
@@ -1,8 +1,8 @@
 const puppeteer = require('puppeteer');
 const path = require('path');
 const { json } = require('body-parser');
+let logs = []; //concating logs because async tasks block from github actions
 
-console.log('ok lets log for github action');
 (async () => {
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
@@ -14,44 +14,44 @@ console.log('ok lets log for github action');
     if(request.url().toString().includes('hb.emxdgt.com')){
 
         //inspect url
-        console.error('EMX Header Bidding Request: ' + request.url());
+        logs.push('EMX Header Bidding Request: ' + request.url());
 
         //inspect json body
         const hbRequest = JSON.parse(request.postData());
         
         //do validation logic here....
-        console.error('checking to ensure EMX header bidding request present');
+        logs.push('checking to ensure EMX header bidding request present');
         if(!hbRequest){
           throw new Error('no header bidding request found');
         }
 
         
-        console.error('checking to ensure request has an id');
+        logs.push('checking to ensure request has an id');
         if(!hbRequest.id){ //check id
           throw new Error('header bidding request must have an id');
         }
 
-        console.error('checking to ensure request has an imp object');
+        logs.push('checking to ensure request has an imp object');
         if(!hbRequest.imp){ //check to make sure containts imp
           throw new Error('header bidding request must have an imp');
         }
 
-        console.error('checking to ensure imp object is an array');
+        logs.push('checking to ensure imp object is an array');
         if(!Array.isArray(hbRequest.imp)){ // make sure that imp object is and array 
           throw new Error('imp object is not an array');
         }
 
-        console.error('checking to ensure imp array is not empty');
+        logs.push('checking to ensure imp array is not empty');
         if(hbRequest.imp.length == 0){ //check it has at least one impression object
           throw new Error('imp array is empty')
         }
 
-        console.error('checking to ensure request is of type site or app');
+        logs.push('checking to ensure request is of type site or app');
         if(!hbRequest.site && !hbRequest.app){ //check to ensure site object or app object, app for education, always site here
           throw new Error('header bidding request must have site or app object')
         }
 
-        console.error(JSON.stringify(hbRequest));
+        logs.push(JSON.stringify(hbRequest));
         
         return;
 
@@ -64,5 +64,9 @@ console.log('ok lets log for github action');
   //load test adapter page
   await page.goto(`file:${path.join(__dirname, './prebid.html')}`);
   await browser.close();
+
+  for(const log of logs){
+    console.log(log);
+  }
  
 })();
