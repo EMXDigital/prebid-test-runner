9aa12d978bb05ca80f9eaa56d7d78ebb29465661
Kevin <kevin.hagens@brealtime.com>
2021-10-20 15:04:27 -0500

    refactor script to log on main thread

--- integration_test.js
+++ integration_test.js
@@ -2,20 +2,9 @@ const puppeteer = require('puppeteer');
 const path = require('path');
 const { json } = require('body-parser');
 
-class IntegrationTest{
-  async run(){
-    //run prebid adapter, get EMX request
-    const request = await this.loadAdapter();
-    if(!request.url){
-      console.error('Failed to get url to EMX header bidding adapter, make sure gulp build command was ran');
-    }
+(async () => {
 
-    //inspect EMX request from adapter
-    console.log('Inspection body for request: ' + request.url);
-    this.inspect(request.body);
-  }
-
-  loadAdapter(){
+  function loadAdapter(){
     return new Promise(async(resolve, reject) => {
       const browser = await puppeteer.launch();
       const page = await browser.newPage();
@@ -50,43 +39,46 @@ class IntegrationTest{
     });
   }
 
-  inspect(body){
-    //do validation logic here....
-    console.log('checking to ensure EMX header bidding request present');
-    if(!body){
-      throw new Error('no header bidding request found');
-    }
-    
-    console.log('checking to ensure request has an id');
-    if(!body.id){ //check id
-      throw new Error('header bidding request must have an id');
-    }
-
-    console.log('checking to ensure request has an imp object');
-    if(!body.imp){ //check to make sure containts imp
-      throw new Error('header bidding request must have an imp');
-    }
-
-    console.log('checking to ensure imp object is an array');
-    if(!Array.isArray(body.imp)){ // make sure that imp object is and array 
-      throw new Error('imp object is not an array');
-    }
-
-    console.log('checking to ensure imp array is not empty');
-    if(body.imp.length == 0){ //check it has at least one impression object
-      throw new Error('imp array is empty')
-    }
-
-    console.log('checking to ensure request is of type site or app');
-    if(!body.site && !body.app){ //check to ensure site object or app object, app for education, always site here
-      throw new Error('header bidding request must have site or app object')
-    }
-
-    console.log(JSON.stringify(body));
-
+  //run prebid adapter, get EMX request
+  const request = await loadAdapter();
+  if(!request.url){
+    console.error('Failed to get url to EMX header bidding adapter, make sure gulp build command was ran');
   }
-}
 
-const integrationTest = new IntegrationTest();
-integrationTest.run();
+  //inspect EMX request from adapter
+  console.log('Inspection body for request: ' + request.url);
 
+  //do validation logic here....
+  console.log('checking to ensure EMX header bidding request present');
+  if(!request.body){
+    throw new Error('no header bidding request found');
+  }
+  
+  console.log('checking to ensure request has an id');
+  if(!request.body.id){ //check id
+    throw new Error('header bidding request must have an id');
+  }
+
+  console.log('checking to ensure request has an imp object');
+  if(!request.body.imp){ //check to make sure containts imp
+    throw new Error('header bidding request must have an imp');
+  }
+
+  console.log('checking to ensure imp object is an array');
+  if(!Array.isArray(request.body.imp)){ // make sure that imp object is and array 
+    throw new Error('imp object is not an array');
+  }
+
+  console.log('checking to ensure imp array is not empty');
+  if(request.body.imp.length == 0){ //check it has at least one impression object
+    throw new Error('imp array is empty')
+  }
+
+  console.log('checking to ensure request is of type site or app');
+  if(!request.body.site && !request.body.app){ //check to ensure site object or app object, app for education, always site here
+    throw new Error('header bidding request must have site or app object')
+  }
+  
+  console.log(JSON.stringify(request.body));
+
+ })()
