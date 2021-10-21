63c2fd686514ecfe9ee52534c7e45ecf61283e34
Kevin <kevin.hagens@brealtime.com>
2021-10-21 09:49:49 -0500

    stable class version, github action still no logs

--- integration_test.js
+++ integration_test.js
@@ -2,96 +2,93 @@ const puppeteer = require('puppeteer');
 const path = require('path');
 const { json } = require('body-parser');
 
-function loadAdapter(){
-  return new Promise(async(resolve, reject) => {
-    const browser = await puppeteer.launch();
-    const page = await browser.newPage();
-    await page.setRequestInterception(true);
+class IntegrationTest{
+  async run(){
+    const request = await this.runAdapter(); //run emx adapter for prebid.js
+    if(!request){
+      throw new Error('Failed to run prebid adapter. Be sure to build gulpfile for project to run correctly');
+    }
 
-    page.on('request', request => {
+    console.log('Inspecting body for request: ' + request.url);
+    this.inspect(request.body); //parse respone ensure good for header bidding proxy server
+  }
 
-      //when header bidding requests go out, capture emx request to server
-      if(request.url().toString().includes('hb.emxdgt.com')){
-
-        //inspect json body
-        const emxRequest = JSON.parse(request.postData());
-        if(!emxRequest){
-          reject(false);
-        }
-
-        resolve({
-          url : request.url(),
-          body : emxRequest 
-        });
-        
-      }else{
-          request.continue();
-      }
+  async runAdapter(){
+    return new Promise(async(resolve, reject) => { //promise turn this into asnyc function
       
-    }); 
-
-    //load test adapter page
-    await page.goto(`file:${path.join(__dirname, './prebid.html')}`);
-    await browser.close();
-
-  });
-}
-
-function getEMXRequest(){
-  return new Promise(async(resolve, reject) => {
-
-
-  });
-}
-
-(async () => {
-
-  //run prebid adapter, get EMX request
-  var request = await loadAdapter();
-  if(!request.url){
-    console.error('Failed to get url to EMX header bidding adapter, make sure gulp build command was ran');
-  }  
-
-  /*var request = {
-    url : 'http://www.google.com',
-    body : '{"test" : "body"}'
-  }; */
-
-  //inspect EMX request from adapter
-  console.log('Inspection body for request: ' + request.url);
-/*
-  //do validation logic here....
-  console.log('checking to ensure EMX header bidding request present');
-  if(!request.body){
-    throw new Error('no header bidding request found');
+      //initialize headless browser
+      const browser = await puppeteer.launch(); 
+      const page = await browser.newPage();
+      await page.setRequestInterception(true);
+      
+      //callbak for when the page starts making request in network console
+      page.on('request', async request => {
+  
+        //when header bidding requests go out, capture emx request to server
+        if(request.url().toString().includes('hb.emxdgt.com')){
+  
+          //inspect json body
+          const emxRequest = JSON.parse(request.postData());
+          if(!emxRequest){
+            reject(false);
+          }
+  
+          resolve({
+            url : request.url(),
+            body : emxRequest 
+          });
+          
+        }else{
+            request.continue();
+        }
+        
+      });
+    
+      //load test adapter page
+      await page.goto(`file:${path.join(__dirname, './prebid.html')}`);
+      await browser.close();
+      
+    });
   }
   
-  console.log('checking to ensure request has an id');
-  if(!request.body.id){ //check id
-    throw new Error('header bidding request must have an id');
+  inspect(body){
+    //do validation logic here....
+    console.log('checking to ensure EMX header bidding request present');
+    if(!body){
+      throw new Error('no header bidding request found');
+    }
+    
+    console.log('checking to ensure request has an id');
+    if(!body.id){ //check id
+      throw new Error('header bidding request must have an id');
+    }
+
+    console.log('checking to ensure request has an imp object');
+    if(!body.imp){ //check to make sure containts imp
+      throw new Error('header bidding request must have an imp');
+    }
+
+    console.log('checking to ensure imp object is an array');
+    if(!Array.isArray(body.imp)){ // make sure that imp object is and array 
+      throw new Error('imp object is not an array');
+    }
+
+    console.log('checking to ensure imp array is not empty');
+    if(body.imp.length == 0){ //check it has at least one impression object
+      throw new Error('imp array is empty')
+    }
+
+    console.log('checking to ensure request is of type site or app');
+    if(!body.site && !body.app){ //check to ensure site object or app object, app for education, always site here
+      throw new Error('header bidding request must have site or app object')
+    }
+    
+    console.log('EMX Prebid Request: ' + '\n' + JSON.stringify(body));
+    process.exit();
   }
+}
 
-  console.log('checking to ensure request has an imp object');
-  if(!request.body.imp){ //check to make sure containts imp
-    throw new Error('header bidding request must have an imp');
-  }
+//run integration test
+const integrationTest = new IntegrationTest();
+integrationTest.run();
 
-  console.log('checking to ensure imp object is an array');
-  if(!Array.isArray(request.body.imp)){ // make sure that imp object is and array 
-    throw new Error('imp object is not an array');
-  }
-
-  console.log('checking to ensure imp array is not empty');
-  if(request.body.imp.length == 0){ //check it has at least one impression object
-    throw new Error('imp array is empty')
-  }
-
-  console.log('checking to ensure request is of type site or app');
-  if(!request.body.site && !request.body.app){ //check to ensure site object or app object, app for education, always site here
-    throw new Error('header bidding request must have site or app object')
-  } */
-
-  console.log(JSON.stringify(request.body));
-  process.exit()
-
- })()
